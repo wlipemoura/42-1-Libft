7ee96001a5a3b4281de31d09b8520fb216b13307
William Moura <felipe.1.william@gmail.com>
2021-09-05 19:13:08 -0300

    memmove correction

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: wfelipe- < wfelipe-@student.42sp.org.br    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/04 19:14:19 by wfelipe-          #+#    #+#             */
-/*   Updated: 2021/09/05 19:01:46 by wfelipe-         ###   ########.fr       */
+/*   Updated: 2021/09/05 19:12:09 by wfelipe-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,21 +14,17 @@
 
 void	*ft_memmove(void *dest, const void *src, size_t n)
 {
-	size_t	cont;
+	unsigned char	*dest2;
+	unsigned char	*src2;
 
-	cont = 0;
-	if (!src && !dest && n > 0)
+	dest2 = (unsigned char *)dest;
+	src2 = (unsigned char *)src;
+	if ((!src || !dest) && n > 0)
 		return (NULL);
 	if (dest == src)
 		return (dest);
-	if (src > dest)
-	{
-		while (cont < n)
-		{
-			*(unsigned char *)(dest + cont) = *(unsigned char *)(src + cont);
-			cont++;
-		}
-	}
+	if (src2 > dest2)
+		ft_memcpy(dest, src, n);
 	else
 	{
 		while (n != 0)
