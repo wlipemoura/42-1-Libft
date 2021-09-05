1ab64b8a9cf81af4ca048cbc66d0d9807862e69e
William Moura <felipe.1.william@gmail.com>
2021-09-05 19:20:26 -0300

    memmove doesn't need any correction

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: wfelipe- < wfelipe-@student.42sp.org.br    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/04 19:14:19 by wfelipe-          #+#    #+#             */
-/*   Updated: 2021/09/05 19:12:09 by wfelipe-         ###   ########.fr       */
+/*   Updated: 2021/09/05 19:19:50 by wfelipe-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,17 +14,19 @@
 
 void	*ft_memmove(void *dest, const void *src, size_t n)
 {
-	unsigned char	*dest2;
-	unsigned char	*src2;
+	size_t	cont;
 
-	dest2 = (unsigned char *)dest;
-	src2 = (unsigned char *)src;
-	if ((!src || !dest) && n > 0)
+	cont = 0;
+	if (!src && !dest && n > 0)
 		return (NULL);
-	if (dest == src)
-		return (dest);
-	if (src2 > dest2)
-		ft_memcpy(dest, src, n);
+	if (src > dest)
+	{
+		while (cont < n)
+		{
+			*(unsigned char *)(dest + cont) = *(unsigned char *)(src + cont);
+			cont++;
+		}
+	}
 	else
 	{
 		while (n != 0)
