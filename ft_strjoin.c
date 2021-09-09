2d6b87c6051942a8a19cb9fc1e7377f64fc23267
William Moura <felipe.1.william@gmail.com>
2021-09-09 18:10:45 -0300

    being safe about overlapping: change memcpy by memmove

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -25,7 +25,7 @@ char	*ft_strjoin(char const *s1, char const *s2)
 	newstring = ft_calloc ((lens1 + lens2 + 1), sizeof (char));
 	if (!newstring)
 		return (NULL);
-	ft_memcpy (newstring, s1, lens1);
-	ft_memcpy (newstring + lens1, s2, lens2);
+	ft_memmove (newstring, s1, lens1);
+	ft_memmove (newstring + lens1, s2, lens2);
 	return (newstring);
 }
